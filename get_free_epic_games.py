07c7b8e5bf5801455fff3f91eb559f93105b0a51
Fasten90 <fasten90@gmail.com>
2024-02-26 21:30:48 +0100

    Get webpage with webbrowser

---
+++ get_free_epic_games.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+curl -s https://store.epicgames.com/en-US/
+
+def link_epic_games_free_game():
+    url = "https://store.epicgames.com/en-US/"
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    link_epic_games_free_game()
+
